cddfde23aad0f44d61b9b068d841f38f6eb8b4b3
Lakshmik22 <136762997+Lakshmik22@users.noreply.github.com>
2023-07-30 19:13:40 -0400

    Update compareSortAlgorithms.c

--- compareSortAlgorithms.c
+++ compareSortAlgorithms.c
@@ -111,20 +111,17 @@ void bubbleSort(int *pData, int n)
 void selectionSort(int *pData, int n)
 {
 	int i, j, min_idx;
-	for (i = 0; i < n - 1; i++)
-	{
-		min_idx = i;
-		for (j = i + 1; j < n; j++)
-		{
-			if (pData[j] < pData[min_idx])
-				min_idx = j;
-		}
-		int temp = pData[min_idx];
-		pData[min_idx] = pData[i];
-		pData[i] = temp;
-		extraMemoryAllocated += 2 * sizeof(int);
-	}
+    for (i = 0; i < n - 1; i++) {
+        min_idx = i;
+        for (j = i + 1; j < n; j++)
+            if (pData[j] < pData[min_idx])
+                min_idx = j;
+        int temp = pData[min_idx];
+        pData[min_idx] = pData[i];
+        pData[i] = temp;
+    }
 }
+
 int memoryAllocated = 0;
 void *allocateMemory(int sz)
 {
